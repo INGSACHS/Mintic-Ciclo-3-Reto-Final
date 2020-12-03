bfdf74a7ab391e25746599d5f98bed1619c65769
ChristianBaezM <39403229+ChristianBaezM@users.noreply.github.com>
2020-12-02 20:17:45 -0500

    Parte II

---
+++ js/admin.js
@@ -0,0 +1,20 @@
+function openSection(evt, sectionName) {
+    var i, x, y;
+    x = document.getElementsByClassName("seccion");
+    y = document.getElementById("welcome");
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(sectionName).style.display = "block";
+    
+    if (y.style.display === "none") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "none";
+    }
+}
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
